e9fe39ac4b26598ccebec53efe159f6a268b71d5
Ivette Hernández <3238938+IvetteLovegood@users.noreply.github.com>
2023-06-16 04:16:14 -0500

    Documentacion lista

--- config/swaggerOptions.js
+++ config/swaggerOptions.js
@@ -3,13 +3,13 @@ const path = require('path');
 module.exports = {
     swaggerDefinition: {
         info: {
-          title: 'Dynamicore Backend API',
-          description: 'Dynamicore Backend API Information',
-          contact: {
-            name: 'Developer Name'
-          },
-          servers: ['http://localhost:8000']
+            title: 'Dynamicore Backend API',
+            description: 'Dynamicore Backend API Information',
+            contact: {
+                name: 'Developer Name'
+            },
+            servers: ['http://localhost:8000']
         }
-      },
-      apis: ['../routes/*.js']
-    };
+    },
+    apis: [path.resolve(__dirname, '../routes/*.js')]
+};

54faf0bc807748e9c88f60f5f27da1fb36f365fa
Ivette Hernández <3238938+IvetteLovegood@users.noreply.github.com>
2023-06-16 04:13:48 -0500

    Ajuste documentacion

--- routes/contacts.js
+++ routes/contacts.js
@@ -2,10 +2,75 @@ const express = require('express');
 const router = express.Router();
 const contactController = require('../controllers/contacts');
 
+/**
+ * @swagger
+ * /contacts:
+ *   get:
+ *     summary: Retrieve a list of contacts
+ *     responses:
+ *       200:
+ *         description: A list of contacts
+ */
 router.get('/', contactController.getContacts);
+
+/**
+ * @swagger
+ * /contacts/{id}:
+ *   get:
+ *     summary: Retrieve a single contact by ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID of the contact
+ *     responses:
+ *       200:
+ *         description: A single contact
+ */
 router.get('/:id', contactController.getContact);
+
+/**
+ * @swagger
+ * /contacts:
+ *   post:
+ *     summary: Create a new contact
+ *     responses:
+ *       200:
+ *         description: The created contact
+ */
 router.post('/', contactController.createContact);
+
+/**
+ * @swagger
+ * /contacts/{id}:
+ *   put:
+ *     summary: Update a contact by ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID of the contact
+ *     responses:
+ *       200:
+ *         description: The updated contact
+ */
 router.put('/:id', contactController.updateContact);
+
+/**
+ * @swagger
+ * /contacts/{id}:
+ *   delete:
+ *     summary: Delete a contact by ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID of the contact
+ *     responses:
+ *       200:
+ *         description: The deleted contact
+ */
 router.delete('/:id', contactController.deleteContact);
+
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,85 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/users');
 
+/**
+ * @swagger
+ * /users:
+ *   get:
+ *     summary: Retrieve a list of users
+ *     responses:
+ *       200:
+ *         description: A list of users
+ */
 router.get('/', userController.getUsers);
+
+/**
+ * @swagger
+ * /users/{id}:
+ *   get:
+ *     summary: Retrieve a single user by ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID of the user
+ *     responses:
+ *       200:
+ *         description: A single user
+ */
 router.get('/:id', userController.getUser);
+
+/**
+ * @swagger
+ * /users:
+ *   post:
+ *     summary: Create a new user
+ *     responses:
+ *       200:
+ *         description: The created user
+ */
 router.post('/', userController.createUser);
+
+/**
+ * @swagger
+ * /users/{id}:
+ *   put:
+ *     summary: Update a user by ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID of the user
+ *     responses:
+ *       200:
+ *         description: The updated user
+ */
 router.put('/:id', userController.updateUser);
+
+/**
+ * @swagger
+ * /users/{id}:
+ *   delete:
+ *     summary: Delete a user by ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID of the user*     responses:
+ *       200:
+ *         description: The deleted user
+ */
 router.delete('/:id', userController.deleteUser);
+
+/**
+ * @swagger
+ * /users/login:
+ *   post:
+ *     summary: Login a user
+ *     responses:
+ *       200:
+ *         description: The logged in user
+ */
 router.post('/login', userController.loginUser);
+
 
 module.exports = router;

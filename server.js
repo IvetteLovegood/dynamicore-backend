0bc3b3756ce81fcf87a4e9dacde814b1ddf41d9d
Ivette Hernández <3238938+IvetteLovegood@users.noreply.github.com>
2023-06-16 03:21:07 -0500

    Documentación swagger

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const app = express();
 const cors = require('cors');
 const usersRoutes = require('./routes/users');
 const contactsRoutes = require('./routes/contacts');
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
 require('dotenv').config();
 const db = require('./db');
 
@@ -24,6 +27,23 @@ app.use(express.json());
 
 app.use('/api/users', usersRoutes);
 app.use('/api/contacts', contactsRoutes);
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    info: {
+      title: 'API de Dynamicore',
+      description: 'Documentación de la API de Dynamicore',
+      contact: {
+        name: 'Soporte de Dynamicore'
+      },
+      servers: ['http://localhost:8000/']
+    }
+  },
+  apis: ['server.js']
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 const PORT = process.env.PORT || 8000;
 
